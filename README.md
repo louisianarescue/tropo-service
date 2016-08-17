b50f9f6db3604f1e3b4e596f854b4caed19efa4b
Benjamin Eckel <bhelx@simst.im>
2016-08-17 11:08:15 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Setup
+
+There are two environment variables to set:
+
+- `PORT` this is the http listen port (defaults to 8000)
+- `DEBUG` if set to true, will print requests and responses (defaults to false)
+
+```
+npm install
+```
---
+++ server.js
@@ -0,0 +1,134 @@
+var tropowebapi = require('tropo-webapi')
+  , express = require('express')
+  , app = express()
+  , bodyParser = require("body-parser")
+  , request = require('request')
+  , debug = process.env.DEBUG || false
+  ;
+
+var messages = {
+  en: {
+    welcome: 'You have contacted the Louisiana Rescue status system',
+    status_code_prompt: 'Please enter your 5 digit status code',
+    repeat_back_code: 'The status code you entered is ',
+    unknown_error: 'Something went wrong, please check that your code is correct and try again in a moment'
+  }
+}
+
+function message(key) {
+  return messages.en[key];
+}
+
+function fetchStatus(code, done) {
+  request({
+      json: true,
+      url: 'http://www.louisianarescue.com/api/rescue/check/' + code,
+      method: 'GET'
+    })
+    .on('response', function(response) {
+      done(null, response.body);
+    })
+    .on('error', function(err) {
+      done(err);
+    });
+}
+
+function tropoResponse(res, tropo) {
+  if (debug) console.log(tropowebapi.TropoJSON(tropo));
+  return res.end(tropowebapi.TropoJSON(tropo));
+}
+
+app.use(bodyParser.json());
+
+app.post('/api/tropo/voice', function(req, res){
+  if (debug) console.dir(req.body);
+
+  var tropo = new tropowebapi.TropoWebAPI();
+  tropo.say(message('welcome'));
+
+  // request 5 digit code
+  var say = new Say(message('status_code_prompt'));
+  var choices = new Choices('[5 DIGITS]');
+
+  // Action classes can be passes as parameters to TropoWebAPI class methods.
+  // use the ask method https://www.tropo.com/docs/webapi/ask.htm
+  tropo.ask(choices, 3, false, null, 'status-response', null, true, say, 5, null);
+
+  // use the on method https://www.tropo.com/docs/webapi/on.htm
+  tropo.on('continue', null, '/api/tropo/voice/answer', true);
+  return tropoResponse(res, tropo);
+});
+
+app.post('/api/tropo/voice/answer', function(req, res){
+  if (debug) console.dir(req.body);
+
+  var tropo = new tropowebapi.TropoWebAPI();
+  var result = req.body.result.actions.interpretation;
+
+  // split up into numbers so she doesn't say "whatever thousand whatver"
+  var statusCodeNumbers = result.split('').join(' ');
+
+  tropo.say(message('repeat_back_code') + statusCodeNumbers);
+
+  fetchStatus(result, function(err, body) {
+    if (debug) console.dir(err);
+    if (debug) console.dir(body);
+
+    if (body && body.success) {
+      tropo.say(body.message);
+      //body.data.status;
+      //body.data.rescued_on;
+      //body.data.updated_on;
+    } else {
+      if (body && body.message) {
+        tropo.say(body.message);
+      } else {
+        if (debug) console.log('something went wrong');
+        tropo.say(message('unknown_error'));
+      }
+    }
+
+    return tropoResponse(res, tropo);
+  });
+});
+
+var statusIdRegex = /\d{5}/;
+
+app.post('/api/tropo/text', function(req, res){
+  if (debug) console.dir(req.body);
+
+  var text = req.body.session.initialText;
+  var userPhone = req.body.session.from.id;
+  var statusMatch = statusIdRegex.exec(text);
+  var tropo = new tropowebapi.TropoWebAPI();
+
+  if (statusMatch) {
+    var statusId = statusMatch[0];
+    if (debug) console.log('status id: ' + statusId);
+
+    fetchStatus(statusId, function(err, body) {
+      if (debug) console.dir(err);
+      if (debug) console.dir(body);
+
+      if (body && body.success) {
+        tropo.say(body.message);
+        //body.data.status;
+        //body.data.rescued_on;
+        //body.data.updated_on;
+      } else {
+        tropo.say(message('unknown_error'));
+      }
+
+      return tropoResponse(res, tropo);
+    });
+  } else {
+    if (debug) console.log('no match for a status id found');
+    tropo.say(message('status_code_prompt'));
+
+    return tropoResponse(res, tropo);
+  }
+});
+
+var port = process.env.PORT || 8000;
+app.listen(port);
+console.log('Server running');
